9e1d6c79dd8af7b9350687d1e21844a5f2ed82c1
CapriciousRebel <code.capriciousrebel@gmail.com>
2021-01-07 11:46:21 +0530

    feat(multithreading): add multithreading to delete from db

--- filesync.py
+++ filesync.py
@@ -2,10 +2,12 @@ import os
 import json
 import requests
 from init import driveinit
+import concurrent.futures
+import time
 
 NEXUS_ROOT = "http://localhost:8005"
 
-
+# APIS
 def listChildFilesInDriveFolder(service, folderID):
     results = service.files().list(
         q="'" + folderID + "' in parents and mimeType != 'application/vnd.google-apps.folder' ",
@@ -15,10 +17,13 @@ def listChildFilesInDriveFolder(service, folderID):
 
 
 def listChildFilesInDB(course, filetype):
-    return requests.get(f"{NEXUS_ROOT}/api/v1/files?course={course}&filetype={filetype}").json()
+    results = requests.get(f"{NEXUS_ROOT}/api/v1/files?course={course}&filetype={filetype}")
+    print(results.status_code)
+    return results.json()
 
 
 def deleteExtraInDB(driveid):
+    print(f"deleting {driveid} from DB")
     return requests.delete("http://localhost:8005/api/v1/files",
                            data={"driveid": driveid})
 
@@ -26,21 +31,19 @@ def deleteExtraInDB(driveid):
 def addExtraToDB(filedata):
     return requests.post("http://localhost:8005/api/v1/files", data=filedata)
 
-
+# FileSync module
 def fileSync(service):
     print('''
-
-
-______ _ _        _____                    _____      _ _   _       _         _ 
-|  ___(_) |      /  ___|                  |_   _|    (_) | (_)     | |       | |
-| |_   _| | ___  \ `--. _   _ _ __   ___    | | _ __  _| |_ _  __ _| |_ ___  | |
-|  _| | | |/ _ \  `--. \ | | | '_ \ / __|   | || '_ \| | __| |/ _` | __/ _ \ | |
-| |   | | |  __/ /\__/ / |_| | | | | (__   _| || | | | | |_| | (_| | ||  __/ |_|
-\_|   |_|_|\___| \____/ \__, |_| |_|\___|  \___/_| |_|_|\__|_|\__,_|\__\___| (_)
-                         __/ |                                                  
-                        |___/                                                   
-
+______ _ _          _____                          _             _      _   _   _ 
+|  ___(_) |        /  ___|                        | |           | |    | | | | | |
+| |_   _| | ___    \ `--. _   _ _ __   ___     ___| |_ __ _ _ __| |_   | | | | | |
+|  _| | | |/ _ \    `--. \ | | | '_ \ / __|   / __| __/ _` | '__| __|  | | | | | |
+| |   | | |  __/   /\__/ / |_| | | | | (__    \__ \ || (_| | |  | |_   |_| |_| |_|
+\_|   |_|_|\___|   \____/ \__, |_| |_|\___|   |___/\__\__,_|_|   \__|  (_) (_) (_)
+                           __/ |                                                  
+                          |___/                                                   
     ''')
+
     print("Loading structure.json...", end=" ")
     input_file = open('structure.json')
     print("Loaded!")
@@ -60,39 +63,63 @@ ______ _ _        _____                    _____      _ _   _       _         _
                     if "_review" not in filetype:
                         print("\nFileType: " + filetype)
                         drive_link = departments[department][course][filetype]
-                        print("Drive_link: " + drive_link)
 
                         filesInDrive = listChildFilesInDriveFolder(
                             service, drive_link)
+                        
                         filesInDB = listChildFilesInDB(course, filetype)
+                        
                         idsInDrive = list(
                             map(lambda file: file['id'], filesInDrive))
                         idsInDB = list(
                             map(lambda file: file['driveid'], filesInDB))
-                        print(f"idsInDrive: {idsInDrive}")
-                        print(f"idsInDB: {idsInDB}")
 
                         # Improve efficiency a lil here
                         extrasInDrive = [
                             item for item in idsInDrive if item not in idsInDB]
                         extrasInDB = [
                             item for item in idsInDB if item not in idsInDrive]
+                        
                         print("Extras in drive: ", extrasInDrive)
                         for id in extrasInDrive:
                             print("Adding " + id + " to db")
                             filedata = {
                                 "title": "logo.jpg",  # TBD
                                 "driveid": id,
-                                "size": "99",  # TBD 
+                                "size": "99",  # TBD
                                 "filetype": filetype,
                                 "finalized": True,
                                 "code": course,
                             }
                             addExtraToDB(filedata)
-                        
-                        print("Extras in db: ", extrasInDB)
-                        for driveid in extrasInDB:
-                            deleteExtraInDB(driveid)
 
+                        print("Extras in db: ", extrasInDB)
+                        with concurrent.futures.ThreadPoolExecutor() as executor:
+                            executor.map(deleteExtraInDB, extrasInDB)
+                            #results = [executor.submit(deleteExtraInDB, driveid) for driveid in extrasInDB]
+                            # for f in concurrent.futures.as_completed(results):
+                            #     print(f.result())
+                        
+                        # for driveid in extrasInDB:
+                        #     print(deleteExtraInDB(driveid))
+
+
+start = time.perf_counter()
 
 fileSync(driveinit())
+
+end = time.perf_counter()
+
+print('''
+
+______ _ _          _____                      _____                       _      _          _   _   _ 
+|  ___(_) |        /  ___|                    /  __ \                     | |    | |        | | | | | |
+| |_   _| | ___    \ `--. _   _ _ __   ___    | /  \/ ___  _ __ ___  _ __ | | ___| |_ ___   | | | | | |
+|  _| | | |/ _ \    `--. \ | | | '_ \ / __|   | |    / _ \| '_ ` _ \| '_ \| |/ _ \ __/ _ \  | | | | | |
+| |   | | |  __/   /\__/ / |_| | | | | (__    | \__/\ (_) | | | | | | |_) | |  __/ ||  __/  |_| |_| |_|
+\_|   |_|_|\___|   \____/ \__, |_| |_|\___|    \____/\___/|_| |_| |_| .__/|_|\___|\__\___|  (_) (_) (_)
+                           __/ |                                    | |                                
+                          |___/                                     |_|                                
+
+''')
+print(f"Syncing time: {round(end - start, 3)} seconds")
