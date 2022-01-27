5dd8fb4237c4ffc596e0535eee5feaa48481a028
Ayan Choudhary <45792768+ayanchoudhary@users.noreply.github.com>
2022-01-27 23:52:13 +0530

    fix nexus URLs

--- studyportal/scripts/load.py
+++ studyportal/scripts/load.py
@@ -7,8 +7,8 @@ ROOT = os.getcwd()
 DEPARTMENTS = os.path.join(ROOT, 'resources/departments.json')
 COURSES = os.path.join(ROOT, 'resources/courses.json')
 
-URL_POST_DEPARTMENTS = "https://nexus.sdslabs.co.in/api/v1/departments/"
-URL_POST_COURSES = "https://nexus.sdslabs.co.in/api/v1/courses/"
+URL_POST_DEPARTMENTS = "https://nexus.sdslabs.co/api/v1/departments/"
+URL_POST_COURSES = "https://nexus.sdslabs.co/api/v1/courses/"
 
 SECRET_KEY = "s-0n5h@4*e((tj3ll%8v=)9t$*24t*mdx6tyt&4+5k-l3x)pl="
 
